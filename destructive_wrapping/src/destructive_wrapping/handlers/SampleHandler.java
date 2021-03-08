f6dd9d1115fa0c55dc4398e25ee0fc940e233a56
RVPKR777 <47763290+RVPKR777@users.noreply.github.com>
2021-03-07 23:14:17 -0800

    pattern update

--- destructive_wrapping/src/destructive_wrapping/handlers/SampleHandler.java
+++ destructive_wrapping/src/destructive_wrapping/handlers/SampleHandler.java
@@ -3,25 +3,45 @@ package destructive_wrapping.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.core.resources.ResourcesPlugin;
 
 public class SampleHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkspace workspace = ResorcesPlugin.getWorkspace();
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IWorkspaceRoot root = workspace.getRoot();
 		IProject[] projects = root.getProjects();
 		for(IProject project : projects) {
 			try {
-				
+				Destructive_catcher(project);
 			}
 			catch(CoreException e){
 				e.printStackTrace();
 			}
 		}
 		return null;
+	}
+	
+	
+	public void Destructive_catcher(IProject project) throws JavaModelException {
+		IPackageFragment[] packages = JavaCore.create(project).getPackageFragments();
+		for(IPackageFragment mypackage : packages) {
+			for(ICompilationUnit unit : mypackage.getCompilationUnits()) {
+				
+			}
+		}
+		
 	}
 }
